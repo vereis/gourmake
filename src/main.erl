0fb7c5eb697fe683ba549b4074ab24d7be1024f3
vereis <me@cbailey.co.uk>
2018-05-02 18:07:34 +0100

    Everything is now OTP, updated readme, removed bootstrap

--- src/main.erl
+++ src/main.erl
@@ -1,110 +1,33 @@
 -module(main).
 -export([
-    main/0,
-    select_ingredient/3
+    main/0
 ]).
 
--define(USE_CUISINE_WHITELIST_PROB, 5).
--define(MUTATIONPROB, 5).
-
 main() ->
-    {Data, Recipes, Names} = bootstrap:bootstrap(),
-    {RecipeName, RecipeIngredients, RecipeInstructions} = parse_recipe(recipe_server:select(), Data),
+    {ok, _} = bootstrap(),
+    
+    % Generate recipe
+    {RecipeName, {RecipeIngredients, RecipeInstructions}} = recipe_server:generate(),
     util:pretty_print_list(RecipeName),
     util:pretty_print_list(RecipeIngredients),
     util:pretty_print_list(RecipeInstructions).
 
-parse_recipe(Recipe, Data) ->
-    #{cuisines := AvailableCuisines} = Recipe,
-    ChosenCuisine = util:pick_random(AvailableCuisines),
-
-    Ingredients = parse_recipe_ingredients(Recipe, ChosenCuisine, Data),
-    Steps = parse_recipe_steps(Recipe, Ingredients),
-    
-    format_output(ChosenCuisine, Recipe, Ingredients, Steps).
-
-parse_recipe_ingredients(Recipe, Cuisine, Data) ->
-    #{ingredients := I} = Recipe,
-    {_, Categories, Cuisines} = Data,
-
-    maps:map(fun(_, {MinQ, MaxQ, Category, FilterCategories}) ->
-        Q = util:pick_random(lists:seq(MinQ, MaxQ)),
-        RawData = [select_ingredient(util:pick_random(Category), Cuisine, FilterCategories) 
-                   || _ <- lists:seq(1, Q)],
-        lists:usort(RawData)
-    end, I).
-
-parse_recipe_name(Recipe, Ingredients) ->
-    #{name := N} = Recipe,
-    interpolate_recipe_string(N, Ingredients).
-
-parse_recipe_steps(Recipe, Ingredients) ->
-    #{steps := S} = Recipe,
-    [interpolate_recipe_string(Step, Ingredients) || Step <- S]. 
-
-interpolate_recipe_string(Str, Ingredients) ->
-    {ok, Regex} = re:compile("\~([a-z_-]+)"),
-    case re:run(Str, Regex, [global]) of
-        nomatch -> {Str, []};
-        {match, Matches} -> 
-            ProcessedStr = binary_to_list(iolist_to_binary([re:replace(Str, Regex, "~s", [global])])),
-            Flags = [list_to_existing_atom(string:slice(Str, M1, M2)) || [_, {M1, M2}] <- Matches],
-            ProcessedFlags = [util:grammatical_concatenate(maps:get(F, Ingredients)) || F <- Flags],
-            {ProcessedStr, ProcessedFlags}
-    end.
-
-format_ingredients(Ingredients) ->
-    IngredientsList = maps:to_list(Ingredients),
-    IngredientStrings = lists:foldl(fun({_, Is}, Acc) ->
-        Acc ++ lists:map(fun(I) ->
-            ["- ", util:pretty_print_atom(I)]
-        end, Is)
-    end, [], IngredientsList),
-    ["## Ingredients:" | IngredientStrings].
-
-format_instructions(Instructions) ->
-    InstructionStrings = lists:map(fun(I) ->
-        {TemplateString, TemplateLiterals} = lists:nth(I, Instructions),
-        [integer_to_list(I), ") ", io_lib:format(TemplateString, TemplateLiterals)]
-    end, lists:seq(1, length(Instructions))),
-    ["## Instructions:" | InstructionStrings].
-
-format_recipe_name(Cuisine, Recipe, Ingredients) ->
-    {RecipeName, RecipeLiterals} = parse_recipe_name(Recipe, Ingredients),
-    CuisineString = case Cuisine of
-        all -> "";
-        _   -> [string:lowercase(util:pretty_print_atom(Cuisine)) | ["-inspired "]]
+%% Start servers if neccessary so that we can generate recipes
+bootstrap() -> 
+    {R1, IngredientServer} = case is_pid(P1 = whereis(ingredient_server)) of
+        true -> {ok, P1};
+        _    -> ingredient_server:start_link()
+    end,
+    {R2, RecipeServer} = case is_pid(P2 = whereis(recipe_server)) of
+        true -> {ok, P2};
+        _    -> recipe_server:start_link()
+    end,
+    {R3, NameServer} = case is_pid(P3 = whereis(name_server)) of
+        true -> {ok, P3};
+        _    -> name_server:start_link()
     end,
 
-    [["# ", CuisineString, io_lib:format(RecipeName, RecipeLiterals)]].
-
-format_output(Cuisine, Recipe, Ingredients, Instructions) ->
-    RecipeName = format_recipe_name(Cuisine, Recipe, Ingredients),
-    IngredientStrs  = format_ingredients(Ingredients),
-    InstructionStrs = format_instructions(Instructions),
-    {RecipeName, IngredientStrs, InstructionStrs}.
-
-pick_recipe(Recipes) ->
-    util:pick_random(Recipes).
-
-select_ingredient(Category, Cuisine, CategoryBlacklist) ->
-    Ingredients = ingredient_server:get(categories, Category),
-    IngredientBlacklist  = lists:usort(lists:flatten([ingredient_server:get(categories, C) || 
-                                                      C <- CategoryBlacklist])),
-    
-    ProcessedIngredients = [I || I <- Ingredients, not lists:member(I, IngredientBlacklist)],
-    
-
-    % We only want to /probabalistically/ apply the cuisine whitelist
-    AvailableIngredients = case rand:uniform(100) > ?USE_CUISINE_WHITELIST_PROB of
-        true -> CuisineIngredients = lists:usort(ingredient_server:get(cuisines, Cuisine) ++ 
-                                                 ingredient_server:get(cuisines, all)),
-                [I || I <- ProcessedIngredients, lists:member(I, CuisineIngredients)];
-        _    -> ProcessedIngredients
-    end,
-   
-    % We also want to make sure AvailableIngredients has at least one element in it
-    case length(AvailableIngredients) > 0 of
-        true -> util:pick_random(AvailableIngredients);
-        _    -> util:pick_random(ProcessedIngredients)
+    case lists:all(fun(X) -> X =:= ok end, [R1, R2, R3]) of
+        true -> {ok, {IngredientServer, RecipeServer, NameServer}};
+        _    -> {err, {start_link_fail, [R1, R2, R3]}}
     end.
--- src/recipe_server.erl
+++ src/recipe_server.erl
@@ -12,11 +12,12 @@
 
 -export([
     stop_link/0,
-    select/0,
+    generate/0,
     get/0,
     start_link/0
 ]).
 
+-define(USE_CUISINE_WHITELIST_PROB, 5).
 
 %%% Entrypoints
 start_link() ->
@@ -28,19 +29,22 @@ stop_link() ->
 get() ->
     gen_server:call(?MODULE, get).
 
-select() ->
-    gen_server:call(?MODULE, select).
+generate() ->
+    gen_server:call(?MODULE, generate).
 
 
 %%% Gen_server call handler
 handle_call(get, _From, DB) ->
     {reply, DB, DB};
 
-handle_call(select, _From, DB) ->
-    {reply, util:pick_random(DB), DB};
+handle_call(generate, _From, DB) ->
+    Recipe = util:pick_random(DB),
+    Reply = generate_recipe(Recipe),
+    {reply, Reply, DB};
 
 handle_call(stop, _From, DB) ->
     {stop, normal, shutdown_ok, DB}.
+
 
 %%% Gen_server boilerplate
 init([]) ->
@@ -58,3 +62,84 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVersion, State, _Extra) ->
     {ok, State}.
+
+
+%%% Private functions
+
+%% Probabalistically generates a random recipe based on an existing recipe template
+generate_recipe(Recipe) ->
+    Cuisine = util:pick_random(maps:get(cuisines, Recipe)),
+    Ingredients = maps:map(fun(_, {Min, Max, Categories, CategoryBlacklist}) ->
+        Quantity = util:pick_random(lists:seq(Min, Max)),
+        Is = [select_ingredient(util:pick_random(Categories), Cuisine, CategoryBlacklist) || _ <- lists:seq(1, Quantity)],
+        lists:usort(Is)
+    end, maps:get(ingredients, Recipe)),
+    Instructions = [interpolate_recipe_string(Instr, Ingredients) || Instr <- maps:get(steps, Recipe)],
+
+    % Now, we want to generate printable strings containing all of the information we've generated
+    % before returning
+    RecipeContents = {pretty_print_ingredients(Ingredients), 
+                      pretty_print_instructions(Instructions)},
+    
+    {name_recipe(Recipe, Cuisine, Ingredients), RecipeContents}.
+
+%% Names a recipe based on the recipe template, cuisine and ingredients
+name_recipe(Recipe, Cuisine, Ingredients) ->
+    {RecipeName, RecipeLiterals} = interpolate_recipe_string(maps:get(name, Recipe), Ingredients),
+    CuisinePrefix = case Cuisine of
+        all -> "";
+        _   -> [util:pretty_print_atom(Cuisine) | [" inspired "]]
+    end,
+    NamePrefix = name_server:get(),
+    [["# ", NamePrefix, CuisinePrefix, io_lib:format(RecipeName, RecipeLiterals)]].
+
+%% Selects ingredients from the ingredient server of a certain category/cuisine
+select_ingredient(Category, Cuisine, CategoryBlacklist) ->
+    Ingredients = ingredient_server:get(categories, Category),
+    IngredientBlacklist  = lists:usort(lists:flatten([ingredient_server:get(categories, C) || 
+                                                      C <- CategoryBlacklist])),
+    
+    ProcessedIngredients = [I || I <- Ingredients, not lists:member(I, IngredientBlacklist)],
+    
+
+    % We only want to /probabalistically/ apply the cuisine whitelist
+    AvailableIngredients = case rand:uniform(100) > ?USE_CUISINE_WHITELIST_PROB of
+        true -> CuisineIngredients = lists:usort(ingredient_server:get(cuisines, Cuisine) ++ 
+                                                 ingredient_server:get(cuisines, all)),
+                [I || I <- ProcessedIngredients, lists:member(I, CuisineIngredients)];
+        _    -> ProcessedIngredients
+    end,
+   
+    % We also want to make sure AvailableIngredients has at least one element in it
+    case length(AvailableIngredients) > 0 of
+        true -> util:pick_random(AvailableIngredients);
+        _    -> util:pick_random(ProcessedIngredients)
+    end.
+
+%% Takes a recipe instruction and interpolates values in place of placeholders in the instruction
+interpolate_recipe_string(Instr, Ingredients) ->
+    {ok, Regex} = re:compile("\~([a-z_-]+)"),
+    case re:run(Instr, Regex, [global]) of
+        nomatch -> {Instr, []};
+        {match, Matches} -> 
+            ProcessedInstr = binary_to_list(iolist_to_binary([re:replace(Instr, Regex, "~s", [global])])),
+            Flags = [list_to_existing_atom(string:slice(Instr, M1, M2)) || [_, {M1, M2}] <- Matches],
+            ProcessedFlags = [util:grammatical_concatenate(maps:get(F, Ingredients)) || F <- Flags],
+            {ProcessedInstr, ProcessedFlags}
+    end.
+
+%% Pretty printing functions
+pretty_print_ingredients(Ingredients) ->
+    IList  = maps:to_list(Ingredients),
+    Output = lists:foldl(fun({_, Is}, Acc) ->
+        Acc ++ [["- " | util:pretty_print_atom(I) ] || I <- Is]
+    end, [], IList),
+    ["## Ingredients:" | Output].
+
+pretty_print_instructions(Instructions) ->
+    Output = lists:map(fun(I) ->
+        {TemplateString, TemplateLiterals} = lists:nth(I, Instructions),
+        [integer_to_list(I), ") ", io_lib:format(TemplateString, TemplateLiterals)]
+    end, lists:seq(1, length(Instructions))),
+    ["## Instructions:" | Output].
+    
